2c3e7021103cb569895d6bd439fef6dcb8fe4720
Sheridan <sheridanmaustad@gmail.com>
2021-05-15 20:48:20 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-function submitData(name, email){
-  const formData = {
-      name: "Steve",
-      email: "steve@steve.com"
-  }
-  const configurationObject = {
-      method: "Post",
-      headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-      },
-      body: JSON.stringify(formData)
-  };
-  return fetch('http://localhost:3000/users',configurationObject)
-  .then(function(response) {
-      return response.json();
+function submitData (userName, userEmail) {
+    //try destructuring => name of arguments, the same as name of keys
+    //TEST 1 - SEND DATA
+    const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        //what will these values be?
+        name: userName,
+        email: userEmail
     })
-    .then(function(object) {
-      console.log(object);
-      //object.id
-      console.log(object.id)
-      document.body.innerHTML = object.id
-      //console.log(document.body)
-    })
-    .catch(function(object){
-        document.body.innerHTML = object.message
 
+} 
+//TEST Test 2 - Handle the Response
+   return fetch('http://localhost:3000/users', configurationObject)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (object) {
+        console.log(object)
+        console.log(object.id)
+        document.body.innerHTML = object.id
+    }) 
+    //Test 3 - Handle Errors
+    .catch (function(object){
+        console.log(object.message)
+        document.body.innerHTML = object.message
     })
 }
-
-submitData("Steve", "steve@steve.com")
